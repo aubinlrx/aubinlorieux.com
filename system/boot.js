2edd24d344f4a450ce9d27ffd9ef54c48fcf0372
Aubin LORIEUX <lorieux@jhd.fr>
2015-03-08 20:57:46 +0100

    Add config port

--- system/boot.js
+++ system/boot.js
@@ -3,8 +3,9 @@
 //Dependencies
 var app = require('../');
 var logger = require('winston');
+var config = require('../application/configs/config');
 
-app.listen(3000);
-logger.info('serveur listening on port 3000');
+app.listen(config.PORT);
+logger.info('serveur listening on port ' + config.PORT);
 
 
